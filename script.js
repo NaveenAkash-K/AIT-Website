3785f31be0cbeac8016d1bd0f0071a0be2b034e7
technaveen <64596821+technaveen@users.noreply.github.com>
2022-10-13 18:32:53 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function func(){
     var x = document.getElementById("sym");
-    var y = document.getElementById("btn").innerHTML="Collapse";
+    var y = document.getElementById("btn").innerHTML="show less";
     if(x.innerHTML==""){
     x.innerHTML="Symposium'22 is a state level symposium hosted by AIT club of Sri Venkateswara College of Engineering, Chennai. The symposium points in creating enterprising abilities while investigating their specialized abilities and improving their reasonable information and thinking of imaginative and remarkable arrangements.";
     }
@@ -13,13 +13,13 @@ function func(){
 
 function boutfunc(){
     var x = document.getElementById("abt");
-    var y = document.getElementById("abtbtn").innerHTML="Collapse";
+    var y = document.getElementById("abtbtn").innerHTML="show less";
     if(x.innerHTML==""){
     x.innerHTML="AIT stands for Association of Information Technologists which is meant for students and faculties only belongs to IT department. This club focuses on developing the standards IT students and as well as the department.  ";
     }
 
     else {
         x.innerHTML="";
-        var y = document.getElementById("abtbtn").innerHTML="about ait club";
+        var y = document.getElementById("abtbtn").innerHTML="click here";
     }
 }
